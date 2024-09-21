3436fb1476f8bff2186a67fa65f39777d350d7a9
POOMIX <92581505+POOMIX@users.noreply.github.com>
2024-09-21 14:37:59 +0700

    up_date_main
    รับจำนวนอุกกาบาตได้แล้ว

--- Main.java
+++ Main.java
@@ -3,6 +3,12 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
+        int amount_meteor = 1;
+        //รับจำนวนอุกกาบาต
+        if (args.length>0) {
+            amount_meteor = Integer.parseInt(args[0]);
+            System.out.println(amount_meteor);
+        }
         JFrame frame = new JFrame();
         frame.setSize(800,600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
