4bbc9006cfd9f2fafbb359833aa11741c308c903
Polite <147123869+politeee123@users.noreply.github.com>
2024-09-24 16:51:15 +0700

    ➕➕

--- panel_meteorito.java
+++ panel_meteorito.java
@@ -2,18 +2,35 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.io.File;
 import java.awt.Graphics;
 import java.util.Random;
 
 
 public class panel_meteorito  extends JPanel {
+    private RandomMeteorito randomMeteorito;
     public panel_meteorito () {
-        setSize(20, 20);
+        randomMeteorito = new RandomMeteorito();
         setBackground(Color.BLACK);
+        setLayout(null);
+    }
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(randomMeteorito.randomImage, 0, 0,randomMeteorito.getNewWidth(),randomMeteorito.getNewHeight(),this);
+        randomMeteorito.repaint();
     }
 }
 
 class RandomMeteorito extends JPanel {
+    Image randomImage;
+    private int newWidth = 50;
+    private int newHeight = 50;
+    public int getNewHeight() {
+        return newHeight;
+    }
+    public int getNewWidth() {
+        return newWidth;
+    }
     String[] imageMeteorito = {
         "images/1.png",
         "images/2.png",
@@ -26,14 +43,10 @@ class RandomMeteorito extends JPanel {
         "images/9.png",
         "images/10.png"
     };
-    
-    Image randomImage;
-
     public RandomMeteorito() {
         Random random = new Random();
         int index = random.nextInt(imageMeteorito.length);
-        randomImage = Toolkit.getDefaultToolkit().getImage(getClass().getClassLoader().getResource(imageMeteorito[index]));
-
+        randomImage = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir")+File.separator+imageMeteorito[index]);
         if (randomImage != null) {
             setSize(randomImage.getWidth(null), randomImage.getHeight(null));
             setLocation(random.nextInt(800), -20);
@@ -42,13 +55,5 @@ class RandomMeteorito extends JPanel {
         }
         
         setOpaque(false);
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        if (randomImage != null) {
-            g.drawImage(randomImage, 0, 0, this);
-        }
     }
 }
