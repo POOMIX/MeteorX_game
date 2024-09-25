1a8a508545c97bc67ab7ba8ad03710f58cb4d8c0
POOMIX <92581505+POOMIX@users.noreply.github.com>
2024-09-25 13:54:14 +0700

    update_motion
    เด้งๆ

--- panel_meteorito.java
+++ panel_meteorito.java
@@ -1,125 +1,128 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.io.File;
 import java.util.Random;
 import javax.swing.JPanel;
 
-
-public class panel_meteorito  extends JPanel {
+public class panel_meteorito extends JPanel {
     private int amount_meteor;
     private RandomMeteorito randomMeteorito;
     private MoveMeteorito[] moveMeteorito;
-    public panel_meteorito (int amount_meteor) {
+
+    public panel_meteorito(int amount_meteor) {
         this.amount_meteor = amount_meteor;
         randomMeteorito = new RandomMeteorito(amount_meteor);
-        moveMeteorito  = new MoveMeteorito[amount_meteor];
+        moveMeteorito = new MoveMeteorito[amount_meteor];
         setBackground(Color.BLACK);
         setLayout(null);
 
         for (int i = 0; i < moveMeteorito.length; i++) {
-            moveMeteorito[i] = new MoveMeteorito(this);
+            moveMeteorito[i] = new MoveMeteorito(this, randomMeteorito.randomImage[i]);
             moveMeteorito[i].start();
         }
-
     }
+
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         for (int i = 0; i < moveMeteorito.length; i++) {
-            g.drawImage(randomMeteorito.randomImage[i], moveMeteorito[i].getX(), moveMeteorito[i].getY(),50,50,this);
-            repaint();
+            g.drawImage(randomMeteorito.randomImage[i], moveMeteorito[i].getX(), moveMeteorito[i].getY(), 50, 50, this);
         }
+        repaint();
+    }
+
+    // เพิ่ม getter สำหรับ moveMeteorito[]
+    public MoveMeteorito[] getMoveMeteorito() {
+        return moveMeteorito;
     }
 }
 
 class RandomMeteorito extends JPanel {
     Image randomImage[];
     String[] imageMeteorito = {
-        "images/1.png",
-        "images/2.png",
-        "images/3.png",
-        "images/4.png",
-        "images/5.png",
-        "images/6.png",
-        "images/7.png",
-        "images/8.png",
-        "images/9.png",
-        "images/10.png"
+        "images/1.png", "images/2.png", "images/3.png", "images/4.png", "images/5.png",
+        "images/6.png", "images/7.png", "images/8.png", "images/9.png", "images/10.png"
     };
+
     public RandomMeteorito(int amount_meteor) {
         Random random = new Random();
-        randomImage  = new Image[amount_meteor];
+        randomImage = new Image[amount_meteor];
         for (int i = 0; i < randomImage.length; i++) {
             int index = random.nextInt(imageMeteorito.length);
-            randomImage[i] = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir")+File.separator+imageMeteorito[index]);
+            randomImage[i] = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir") + File.separator + imageMeteorito[index]);
         }
-        
         setOpaque(false);
     }
 }
-class MoveMeteorito extends Thread{
+
+class MoveMeteorito extends Thread {
     private int x = 400;
     private int y = 300;
+    private int vx = 1;
+    private int vy = 1;
     private panel_meteorito panel;
+    private Image meteorImage;
     private Random random = new Random();
-    private int randomdirection;
-    MoveMeteorito(panel_meteorito panel){
+
+    MoveMeteorito(panel_meteorito panel, Image meteorImage) {
         this.panel = panel;
-
-        randomdirection = random.nextInt(8);
+        this.meteorImage = meteorImage;
+        vx = random.nextInt(3) + 1; // ความเร็วในแกน X (สุ่ม)
+        vy = random.nextInt(3) + 1; // ความเร็วในแกน Y (สุ่ม)
     }
-    public void move(){
-        if (randomdirection == 0) {//right
-            x++;
-        } else if (randomdirection == 1){//down
-            y++;
-        } else if(randomdirection == 2){//left
-            x--;
-        } else if(randomdirection == 3){//up
-            y--;
-        } else if (randomdirection == 4){//upright
-            x++;
-            y--;
-        } else if(randomdirection == 5){//upleft
-            x--;
-            y--;
-        } else if(randomdirection == 6){//downright
-            x++;
-            y++;
-        } else if(randomdirection == 7){//downleft
-            x--;
-            y++;
+
+    public void move() {
+        // เคลื่อนที่วัตถุ
+        x += vx;
+        y += vy;
+
+        // การตรวจสอบขอบเขตของจอ
+        if (x >= panel.getWidth() - 50 || x <= 0) {
+            vx = -vx; // เด้งในแกน X
         }
-
-        if (x >= 730) {
-            randomdirection = 2;
-        } else if (y >= 530) {
-            randomdirection = 3;
-        } else if (x <= 0) {
-            randomdirection = 0;
-        } else if (y <= 0) {
-            randomdirection = 1;
-        } 
+        if (y >= panel.getHeight() - 50 || y <= 0) {
+            vy = -vy; // เด้งในแกน Y
+        }
     }
+
+    public void detectCollision(MoveMeteorito other) {
+        Rectangle rect1 = new Rectangle(x, y, 50, 50);
+        Rectangle rect2 = new Rectangle(other.getX(), other.getY(), 50, 50);
+
+        if (rect1.intersects(rect2)) {
+            // สลับทิศทางเมื่อชนกัน
+            vx = -vx;
+            vy = -vy;
+            other.vx = -other.vx;
+            other.vy = -other.vy;
+        }
+    }
+
     public int getX() {
         return x;
     }
+
     public int getY() {
         return y;
     }
+
     @Override
     public void run() {
         while (true) {
             try {
-                Thread.sleep(1);
+                Thread.sleep(10);
                 move();
+                for (int i = 0; i < panel.getMoveMeteorito().length; i++) {
+                    if (panel.getMoveMeteorito()[i] != this) {
+                        detectCollision(panel.getMoveMeteorito()[i]);
+                    }
+                }
             } catch (Exception e) {
-                // TODO: handle exception
+                e.printStackTrace();
             }
         }
     }
-    public void Detection(){
-
-    }
 }
+
