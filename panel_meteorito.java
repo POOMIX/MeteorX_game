8587c4f6be908c20f4f9069dccade18270867f50
Polite <147123869+politeee123@users.noreply.github.com>
2024-09-27 22:08:39 +0700

    ➕

--- panel_meteorito.java
+++ panel_meteorito.java
@@ -6,11 +6,11 @@ import java.io.File;
 import java.awt.Graphics;
 import java.util.Random;
 
-
-public class panel_meteorito  extends JPanel {
+public class panel_meteorito extends JPanel {
     private RandomMeteorito randomMeteorito;
-    private MoveMeteorito[] moveMeteorito = new MoveMeteorito[5];;
-    public panel_meteorito () {
+    private MoveMeteorito[] moveMeteorito = new MoveMeteorito[5];
+
+    public panel_meteorito() {
         randomMeteorito = new RandomMeteorito();
 
         setBackground(Color.BLACK);
@@ -20,109 +20,120 @@ public class panel_meteorito  extends JPanel {
             moveMeteorito[i] = new MoveMeteorito(this);
             moveMeteorito[i].start();
         }
-
     }
+
+    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         for (int i = 0; i < moveMeteorito.length; i++) {
-            g.drawImage(randomMeteorito.randomImage[i], moveMeteorito[i].getX(), moveMeteorito[i].getY(),50,50,this);
-            repaint();
+            g.drawImage(randomMeteorito.randomImage[i], moveMeteorito[i].getX(), moveMeteorito[i].getY(), 50, 50, this);
         }
     }
 }
 
 class RandomMeteorito extends JPanel {
-    Image randomImage[] = new Image[5];;
+    Image randomImage[] = new Image[5];
     String[] imageMeteorito = {
-        "images/1.png",
-        "images/2.png",
-        "images/3.png",
-        "images/4.png",
-        "images/5.png",
-        "images/6.png",
-        "images/7.png",
-        "images/8.png",
-        "images/9.png",
-        "images/10.png"
+        "images/1.png", "images/2.png", "images/3.png",
+        "images/4.png", "images/5.png", "images/6.png",
+        "images/7.png", "images/8.png", "images/9.png", "images/10.png"
     };
+
     public RandomMeteorito() {
         Random random = new Random();
         for (int i = 0; i < randomImage.length; i++) {
             int index = random.nextInt(imageMeteorito.length);
-            randomImage[i] = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir")+File.separator+imageMeteorito[index]);
+            randomImage[i] = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir") + File.separator + imageMeteorito[index]);
         }
-        
+
         setOpaque(false);
     }
 }
-class MoveMeteorito extends Thread{
+
+class MoveMeteorito extends Thread {
     private int x = 400;
     private int y = 300;
     private panel_meteorito panel;
     private Random random = new Random();
     private int randomdirection;
-    MoveMeteorito(panel_meteorito panel){
+    private int speed;  // Speed of the meteor
+    private int[] random257 = {2, 5, 7};
+    private int[] random046 = {0, 4, 6};
+    private int[] random345 = {3, 4, 5};
+    private int[] random176 = {1, 7, 6};
+
+    MoveMeteorito(panel_meteorito panel) {
         this.panel = panel;
-
-        randomdirection = random.nextInt(8);
+        randomdirection = random.nextInt(8);  // Random initial direction
+        speed = random.nextInt(5) + 1;  // Random speed between 1 and 5
     }
-    public void move(){
-        if (randomdirection == 0) {//right
-            x++;
-        } else if (randomdirection == 1){//down
-            y++;
-        } else if(randomdirection == 2){//left
-            x--;
-        } else if(randomdirection == 3){//up
-            y--;
-        } else if (randomdirection == 4){//upright
-            x++;
-            y--;
-        } else if(randomdirection == 5){//upleft
-            x--;
-            y--;
-        } else if(randomdirection == 6){//downright
-            x++;
-            y++;
-        } else if(randomdirection == 7){//downleft
-            x--;
-            y++;
+
+    public void move() {
+        if (randomdirection == 0) {  // right
+            x = x + speed;
+        } else if (randomdirection == 1) {  // down
+            y = y + speed;
+        } else if (randomdirection == 2) {  // left
+            x = x - speed;
+        } else if (randomdirection == 3) {  // up
+            y = y - speed;
+        } else if (randomdirection == 4) {  // upright
+            x = x + speed;
+            y = y - speed;
+        } else if (randomdirection == 5) {  // upleft
+            x = x - speed;
+            y = y - speed;
+        } else if (randomdirection == 6) {  // downright
+            x = x + speed;
+            y = y + speed;
+        } else if (randomdirection == 7) {  // downleft
+            x = x - speed;
+            y = y + speed;
         }
 
-        if (x >= panel.getWidth()-50) {
-            if (randomdirection == 0) randomdirection = 2; // From right to left
-            else if (randomdirection == 4) randomdirection = 5; // From upright to upleft
-            else if (randomdirection == 6) randomdirection = 7; // From downright to downleft
+        // Handle boundary conditions
+        if (x >= panel.getWidth() - 50) {
+            if (randomdirection == 0 || randomdirection == 4 || randomdirection == 6) {
+                randomdirection = random257[random.nextInt(random257.length)];  // Choose from leftward directions
+                speed = random.nextInt(5) + 1;
+            }
         } else if (x <= 0) {
-            if (randomdirection == 2) randomdirection = 0; // From left to right
-            else if (randomdirection == 5) randomdirection = 4; // From upleft to upright
-            else if (randomdirection == 7) randomdirection = 6; // From downleft to downright
+            if (randomdirection == 2 || randomdirection == 5 || randomdirection == 7) {
+                randomdirection = random046[random.nextInt(random046.length)];  // Choose from rightward directions
+                speed = random.nextInt(5) + 1;
+            }
         }
-    
-        if (y >= panel.getHeight()-50) {
-            if (randomdirection == 1) randomdirection = 3; // From down to up
-            else if (randomdirection == 6) randomdirection = 5; // From downright to upleft
-            else if (randomdirection == 7) randomdirection = 4; // From downleft to upright
+
+        if (y >= panel.getHeight() - 50) {
+            if (randomdirection == 1 || randomdirection == 7 || randomdirection == 6) {
+                randomdirection = random345[random.nextInt(random345.length)];  // Choose upward directions
+                speed = random.nextInt(5) + 1;
+            }
         } else if (y <= 0) {
-            if (randomdirection == 3) randomdirection = 1; // From up to down
-            else if (randomdirection == 4) randomdirection = 7; // From upright to downleft
-            else if (randomdirection == 5) randomdirection = 6; // From upleft to downright
+            if (randomdirection == 3 || randomdirection == 4 || randomdirection == 5) {
+                randomdirection = random176[random.nextInt(random176.length)];  // Choose downward directions
+                speed = random.nextInt(5) + 1;
+            }
         }
     }
+
     public int getX() {
         return x;
     }
+
     public int getY() {
         return y;
     }
+
     @Override
     public void run() {
         while (true) {
             try {
-                Thread.sleep(10);
+                Thread.sleep(10);  // Control update rate
                 move();
+                panel.repaint();  // Repaint after moving
             } catch (Exception e) {
-                // TODO: handle exception
+                e.printStackTrace();
             }
         }
     }
